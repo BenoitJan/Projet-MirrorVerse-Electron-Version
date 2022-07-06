0e15710c8202185fa9dc285cf93cf4cab084c283
Adrien Calmesnil <adcalmesnil@gmail.com>
2022-07-06 12:35:34 +0200

    Spotify js management

---
+++ gui/linkers/spotify.js
@@ -0,0 +1,16 @@
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  let element = document.getElementById("embed-iframe");
+  let options = {
+    uri: "spotify:track:6iq7kLF6m5uhr5vE0ow0hC?si=5c0b15c9e8b047a9",
+  };
+  let callback = (EmbedController) => {
+    document
+      .querySelectorAll("ul#episodes > li > button")
+      .forEach((episode) => {
+        episode.addEventListener("click", () => {
+          EmbedController.loadUri(episode.dataset.spotifyId);
+        });
+      });
+  };
+  IFrameAPI.createController(element, options, callback);
+};
